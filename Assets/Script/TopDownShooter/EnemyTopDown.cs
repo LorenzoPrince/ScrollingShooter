091718cd3f022baf0cd53a86a07c5ffcb2d85090
LorenzoPrince <lorenzoprince222@gmail.com>
2025-07-16 03:29:36 -0300

    NavMesh
    Cree el navMesh para que sigan al jugador que tenga el tag especifico.

--- Assets/Script/TopDownShooter/EnemyTopDown.cs
+++ Assets/Script/TopDownShooter/EnemyTopDown.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemyTopDown : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null)
+        {
+            Debug.LogError("No se encontró al jugador con el tag 'Player'");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+            Debug.Log("El enemigo está siguiendo al jugador: " + player.position);
+        }
+        else
+        {
+            Debug.LogWarning("El jugador no se ha encontrado");
+        }
     }
 }
