cc66719787c5e53d25226bf9f5a1df63881a76e0
LorenzoPrince <lorenzoprince222@gmail.com>
2025-07-28 03:48:34 -0300

    Finish
    Arregle errores en el top down y le agregue sonidos

--- Assets/Script/TopDownShooter/ShootPlayerTopDown.cs
+++ Assets/Script/TopDownShooter/ShootPlayerTopDown.cs
@@ -4,10 +4,12 @@ using UnityEngine.InputSystem;
 
 public class ShootPlayerTopDown : ShootingTopDown
 {
+
     public void Onfire(InputAction.CallbackContext context)
     {
         if (context.performed) // que se active sol9 una cvez cyando toque
         {
+
             Shoot();
         }
     }
--- Assets/Script/TopDownShooter/ShootingTopDown.cs
+++ Assets/Script/TopDownShooter/ShootingTopDown.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
-
+using System.Collections;
 public class ShootingTopDown : MonoBehaviour
 {
+    public AudioSource blaster;
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float bulletSpeed = 20f;
-  
+    private float shootCooldown = 1.5f; // Tiempo entre disparos
+    private bool puedeDisparar = true;
 
-
-    public void Shoot() // si no pongo que es unity lo toma como privado
+    public void Shoot()
     {
-        if (bulletPrefab == null || firePoint == null) return;
+        if (!puedeDisparar) return;
+        StartCoroutine(DisparoConCooldown());
+    }
+    IEnumerator DisparoConCooldown() // si no pongo que es unity lo toma como privado
+    {
+        puedeDisparar = false;
+        if (bulletPrefab == null || firePoint == null) yield break;
+
 
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
+        blaster.Play();
         bullet.transform.rotation = Quaternion.Euler(0, 0, 90); 
 
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
@@ -23,8 +31,10 @@ public class ShootingTopDown : MonoBehaviour
             rb.linearVelocity = firePoint.forward * bulletSpeed;
         }
 
-        Destroy(bullet, 4f);
 
+        Destroy(bullet, 4f);
+        yield return new WaitForSeconds(shootCooldown);
+        puedeDisparar = true;
     }
 }
 
